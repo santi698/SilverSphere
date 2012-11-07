6a074ac1411e4a55e5af3760cc579afa9af48d52
eugis <me.sakuda@hotmail.com>
2012-11-07 17:39:51 +0100

    Falta solucionar dos cosas del test. están comentados las fallas

--- tests/tests/TestMove.java
+++ tests/tests/TestMove.java
@@ -1,5 +1,6 @@
 package tests;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
@@ -9,6 +10,7 @@ import org.junit.Test;
 import board.Board;
 import board.Direction;
 import board.InvalidLevelException;
+import board.MoveRes;
 import board.Position;
 import cell.Box;
 import cell.Character;
@@ -27,8 +29,8 @@ import cell.Character;
 			Board board1 = new Board(sArr);
 			Direction dir1 = Direction.RIGHT;
 			ArrayList<Position> positionList = board1.moveCharacter(dir1);
-			int x = 0; // falta definir x e y en funciï¿½n de 
-			int y = 1; //
+			int x = 0; 
+			int y = 1;
 			assertTrue(board1.getCell(x + 1, y + 0).getContent() instanceof Character);
 			assertTrue(positionList.contains(new Position(1 + x, y + 0))); 
 			assertTrue(board1.getCell(x + 2, y + 0).getContent() instanceof Box); 
@@ -37,7 +39,7 @@ import cell.Character;
 		@Test
 		public void wrongMove() throws InvalidLevelException{
 			String[] sArr2 = new String[3];
-			sArr2[0]= "B G";
+			sArr2[0]= "BBG";
 			sArr2[1]= "@T ";
 			sArr2[2]= "C K";
 		
@@ -61,5 +63,42 @@ import cell.Character;
 			assertTrue(board2.moveCharacter(dirTree).isEmpty());
 			assertTrue(board2.moveCharacter(dirInmobileBox).isEmpty());
 			assertTrue(board2.moveCharacter(dirIce).isEmpty());	
+			assertTrue(board2.getCell( 0 , 0 ).getContent().move(board2, Direction.RIGHT).isEmpty());	
+		}
+		
+		
+		@Test
+		public void terminalMove() throws InvalidLevelException{
+			
+			String[] sArr3 = new String[4];
+			sArr3[0]= "B BG";
+			sArr3[1]= "@#KB";
+			sArr3[2]= "B CB";
+			sArr3[3]= "#   ";	
+			
+			Board board3 = new Board(sArr3);
+			
+			
+			//Revisar las direcciones.
+			
+			board3.moveCharacter(Direction.DOWN);
+			assertEquals(board3.getCell( 0 , 3 ).toString(), "Floating Box");
+			
+			board3.moveCharacter(Direction.RIGHT);
+			board3.moveCharacter(Direction.DOWN);
+			board3.moveCharacter(Direction.RIGHT);
+			board3.moveCharacter(Direction.UP);
+			//assertEquals(board3.getCell( 2 , 1 ).toString(), "Interruptor +" + board3.getCell( 2 , 1 ).getContent() );
+			
+			//assertEquals(board3.checkMove(board3.moveCharacter(Direction.RIGHT)),  MoveRes.WATER_REACHED);
+
+		
+			/*
+			 * Falta resolver los Tres comentarios de arriba. Después creo que todo está cubierto.
+			 */
+			
+			
+			
+			
 		}
 }
